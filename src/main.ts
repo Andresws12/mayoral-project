034e574d5b73875fb5d997e0c4539edcff4286ff
afhg <andres.hernandez@star-group.net>
2021-02-04 15:39:41 +0100

    Configuration project: Configure all dependencies and project to start

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import Vue from "vue";
 import App from "./App.vue";
-import "./registerServiceWorker";
 import router from "./router";
-import store from "./store";
+
+import { store } from "./store";
+
+import Router from "vue-router";
+
+import "./styles/main.scss";
+
+import Buefy from "buefy";
 
 Vue.config.productionTip = false;
+
+Vue.use(Router);
+Vue.use(Buefy, {
+  defaultIconPack: "fas",
+});
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
