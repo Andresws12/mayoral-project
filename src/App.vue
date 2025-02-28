14ffe4a668324f6942ba4854c21803fd8b40b9cc
Andres Hernandez <andres.hernandez@iskaypet.com>
2025-03-01 00:58:11 +0100

    Add github button

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
     <router-view />
+    <GithubButton />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-
+import GithubButton from "@/components/GithubButton.vue";
 @Component({
   name: "App",
+  components: {
+    GithubButton,
+  },
 })
 export default class App extends Vue {}
 </script>
---
+++ src/components/GithubButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <a
+    class="github-button"
+    href="https://github.com/Andresws12/mayoral-project"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <img src="@/assets/github.svg" alt="Github" />
+  </a>
+</template>
+
+<style lang="scss" scoped>
+.github-button {
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  z-index: 20;
+  width: 40px;
+  height: 40px;
+}
+</style>
